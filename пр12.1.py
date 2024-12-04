1f84bded31615ea37fb8d5fadbf26dfb09198864
Egor648 <mendelev97@bk.ru>
2024-12-04 22:21:35 +0300

    Add files via upload

---
+++ пр12.1.py
@@ -0,0 +1,12 @@
+def sum_of_digits(N):
+    total = 0  
+    while N > 0:
+        digit = N % 10  
+        total += digit   
+        N //= 10         
+    return total
+
+# Ввод числа от пользователя
+N = int(input("Введите натуральное число: "))
+result = sum_of_digits(N)
+print("Сумма цифр числа:", result)
